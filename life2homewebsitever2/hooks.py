96d64652061cbac8a4185a99b65f6b180234512f
Hemant Savkar <>
2024-01-02 14:26:57 +0530

    Added Frappe.call support from our WebSite.

--- life2homewebsitever2/hooks.py
+++ life2homewebsitever2/hooks.py
@@ -13,6 +13,7 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/life2homewebsitever2/css/life2homewebsitever2.css"
 # app_include_js = "/assets/life2homewebsitever2/js/life2homewebsitever2.js"
+app_include_js = "/templates/pages/page.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/life2homewebsitever2/css/life2homewebsitever2.css"
