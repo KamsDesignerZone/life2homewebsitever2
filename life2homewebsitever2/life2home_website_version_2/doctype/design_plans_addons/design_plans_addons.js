76be303105cf0533eeedd2008c97c8ccfc3de600
Hemant Savkar <>
2024-02-18 13:29:38 +0530

    Added New Docktypes

---
+++ life2homewebsitever2/life2home_website_version_2/doctype/design_plans_addons/design_plans_addons.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Kams Designer Zone and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Design Plans Addons', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ life2homewebsitever2/life2home_website_version_2/doctype/design_plans_addons/design_plans_addons.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Kams Designer Zone and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class DesignPlansAddons(Document):
+	pass
